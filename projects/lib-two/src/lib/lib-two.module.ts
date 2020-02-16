5c68417853a223c396c78f700797e0b948b4f138
Jonathan Chase <JonathanChase@outlook.com>
2020-02-16 17:11:32 +0900

    added dep on lib-one to lib-two

--- projects/lib-two/src/lib/lib-two.module.ts
+++ projects/lib-two/src/lib/lib-two.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import { LibOneModule } from 'lib-one';
 import { Comp2Component } from './comp2/comp2.component';
 
 @NgModule({
   declarations: [Comp2Component],
-  imports: [],
+  imports: [LibOneModule],
   exports: [Comp2Component]
 })
 export class LibTwoModule {}
